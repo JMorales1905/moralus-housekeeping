b8c6a1136a2549d3e5e3848e24ece9a5f61a24dd
JMorales1905 <Xweyin.21@gmail.com>
2025-01-09 22:04:40 -0800

    fist commmit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { color } from "framer-motion";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -11,6 +12,11 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'red': '#900000',
+        'red-1': '#ee2400',
+        'light': '#ffb09c',
+        'light-1': '#fbd9d3',
+        'light-2': '#ffefea',
       },
     },
   },
