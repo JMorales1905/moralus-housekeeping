18d57e27ce4748067756856a593a2f210a48f777
JMorales1905 <Xweyin.21@gmail.com>
2025-01-15 13:11:04 -0800

    Updated some fonts. Nav bard fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,27 @@
 import Image from "next/image";
+import { Open_Sans } from "next/font/google";
+
+const sans = Open_Sans({ subsets: ['latin'] })
 
 export default function Home() {
   return (
 
-    <div>
+    <div className={sans.className}>
       {/* Navbar will have two sections, Left side will be logo and right side will be menus */}
-      <nav className="bg-white text-black flex h-[5vh] items-center">
-        <section className="flex-1 ml-8">Logo</section>
-        <section className="flex-1 flex justify-evenly">
-          <a href="">Home</a>
-          <a href="#about-us">About Us</a>
-          <a href="#services">Services</a>
-        </section>
+      <nav className="h-[54px]" id="home">
+        <div className="bg-white text-black flex w-[100%] h-[5vh] items-center fixed shadow-md shadow-black/5">
+          <section className="flex-1 ml-8">Logo</section>
+          <section className="flex-1 flex justify-evenly text-[20px]">
+            <a href="#home">Home</a>
+            <a href="#about-us">About Us</a>
+            <a href="#services">Services</a>
+          </section>
+        </div>
+
       </nav>
 
       <div className="text-black h-[10vh]">
-        <div className="grid justify-center bg-light-2 h-full rounded-md items-center text-[40px]">
+        <div className="grid justify-center bg-light-2 h-full rounded-md items-center text-[40px] font-semibold">
           Moralus Housekeeping Services
         </div>
 
@@ -37,9 +43,9 @@ export default function Home() {
               After immigrating from Guatemala, Blanca found herself in the land of dreams and
               after working with a cleaning company for two years, decided to take her personal approach
               to the business. Now, Blanca works with her own clients who she has earned the trust of
-              and deliveres her cleaning services to them on a weekly/monthly basis. Blanca would be honored
-              to provide you with twelve years of housekeeping service experience and a personal experience you
-              wont find with large-scale cleaning companies. She brings to the table services you could only find
+              and provides her cleaning services to them on a weekly/monthly basis. Blanca would be honored
+              to provide you with twelve years of housekeeping service and a personal experience you
+              won't find with large-scale cleaning companies. She brings to the table services you could only find
               with her family, where Blanca take pride in the cleanliness of her home.
             </p>
 
@@ -63,14 +69,14 @@ export default function Home() {
                 {/* social media links */}
               </div>
             </div>
-            <p className="text-center">Schedule a quote over the phone or for a more peronal experience, let us visit your home in person. <br />
+            <p className="text-center text-[20px]">Schedule a quote over the phone or for a more peronal experience, let us visit your home in person. <br />
               Tell us what services you desire and what day of the week works best for you and we'll give you a quote.
               <br />Of course, it's <span className="text-[20px]">FREE!</span></p>
 
           </div>
         </section>
 
-        <footer className="bg-white flex justify-evenly">
+        <footer className="bg-white flex justify-evenly p-6 items-center">
           <div>
             <h2 className="text-[25px]">Company logo</h2>
             <p> We would feel privileged to service you!</p>
@@ -80,13 +86,13 @@ export default function Home() {
           <div>
             <h2 className="text-[25px]">Call Hours</h2>
             <ul>
-              <li className="border-black border-b-[1px]">Monday: 3pm-9pm</li>
-              <li className="border-black border-b-[1px]">Tuesday: 3pm-9pm</li>
-              <li className="border-black border-b-[1px]">Wednesday: 3pm-9pm</li>
-              <li className="border-black border-b-[1px]">Thursday: 3pm-9pm</li>
-              <li className="border-black border-b-[1px]">Friday: 3pm-9pm</li>
-              <li className="border-black border-b-[1px]">Saturday: 3pm-9pm</li>
-              <li className="border-black border-b-[1px]">Sunday: 3pm-9pm</li>
+              <li className="border-black border-b-[1px]">Monday: 3pm - 9pm</li>
+              <li className="border-black border-b-[1px]">Tuesday: 3pm - 9pm</li>
+              <li className="border-black border-b-[1px]">Wednesday: 3pm - 9pm</li>
+              <li className="border-black border-b-[1px]">Thursday: 3pm - 9pm</li>
+              <li className="border-black border-b-[1px]">Friday: 3pm - 9pm</li>
+              <li className="border-black border-b-[1px]">Saturday: 3pm - 9pm</li>
+              <li className="border-black border-b-[1px]">Sunday: 3pm - 9pm</li>
             </ul>
           </div>
         </footer>
